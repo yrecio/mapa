b4fe437b4fee8cb30f42981285003d3f746d4287
Yon <yonrecio@gmail.com>
2022-10-17 16:28:39 +0200

    mapa

---
+++ layers/layers.js
@@ -0,0 +1,17 @@
+var wms_layers = [];
+
+
+        var lyr_ESRISatelliteArcGISWorld_Imagery_0 = new ol.layer.Tile({
+            'title': 'ESRI Satellite (ArcGIS/World_Imagery)',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+            })
+        });
+
+lyr_ESRISatelliteArcGISWorld_Imagery_0.setVisible(true);
+var layersList = [lyr_ESRISatelliteArcGISWorld_Imagery_0];
